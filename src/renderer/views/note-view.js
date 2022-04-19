7fe12517f7baac8f0f3e2c3d3bd1be99f9abe281
Komichi <k0michi@koyomi.co>
2022-04-19 15:08:41 +0900

    Show note's title

--- src/renderer/views/note-view.js
+++ src/renderer/views/note-view.js
@@ -15,6 +15,10 @@ export default class NoteView {
 
   render(note) {
     utils.removeChildNodes(this.$noteContent);
+    const $h1 = document.createElement('h1');
+    $h1.id = 'title';
+    $h1.textContent = note.head.properties.title;
+    this.$noteContent.append($h1);
 
     for (const e of note.content) {
       this.$noteContent.append(e.element);
