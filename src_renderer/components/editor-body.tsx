ffe64b42778e20a4a3721b2adb10757e2a8f7dd5
Komichi <k0michi@koyomi.co>
2022-06-12 19:51:59 +0900

    Focus the last character of block

--- src_renderer/components/editor-body.tsx
+++ src_renderer/components/editor-body.tsx
@@ -9,8 +9,11 @@ export default function EditorBody(props: EditorBodyProps) {
 
   return (
     <div ref={editorBody} id="editor-body" onClick={e => {
-      if (e.target == editorBody.current) {
-        (editorBody.current?.lastChild! as any)?.focus();
+      if (e.target == editorBody.current && editorBody.current?.lastChild != null) {
+        const target =  editorBody.current?.lastChild as any;
+        target.focus();
+        window.getSelection()?.selectAllChildren(target);
+        window.getSelection()?.collapseToEnd();
       }
     }}>
       {props.element}
